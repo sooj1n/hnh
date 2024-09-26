973ddd2efd92d7ae1d2c85c445e079ce40437de0
sooj1n <sujin19371@gmail.com>
2024-09-26 15:26:38 +0900

    fix

--- src/hnh/main.py
+++ src/hnh/main.py
@@ -1,9 +1,11 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
-
 import random
+from transformers import pipeline
+import io
+from hnh.utils import get_max_label
 
 app = FastAPI()
 
@@ -23,5 +25,23 @@ async def home(request: Request):
 
 @app.get("/predict")
 def hotdog():
-    
+    model = pipeline("image-classification", model="julien-c/hotdog-not-hotdog") 
     return {"Hello": random.choice(["hotdog", "not hotdog"])}
+
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    # 파일 저장
+    img = await file.read()
+    model = pipeline("image-classification", model="julien-c/hotdog-not-hotdog")
+    
+    from PIL import Image
+    img = Image.open(io.BytesIO(img))  # 이미지 바이트를 PIL 이미지로 변환
+    
+    p = model(img)
+    label = get_max_label(p)
+    #{'label': 'hot dog', 'score': 0.54},
+    #{'label': 'not hot dog', 'score': 0.46}
+
+    return {"label": label, "p": p} 
